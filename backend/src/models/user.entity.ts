6455149089d000d601ce7e5ae421ab49e4661215
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-07-24 15:31:23 +0400

    Authentication & RBAC Implementation

---
+++ backend/src/models/user.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'jsonb', default: ['user'] })
+  roles: string[];
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
